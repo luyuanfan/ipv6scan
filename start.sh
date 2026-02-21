c0b3760d5db8a58bab3bc94c28b364130c1dbecc
Jasmine Fan <lyspfan@gmail.com>
2026-02-21 17:53:35 -0500

    update start script with nohup

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 main.py /mnt/usb/combined-48s-r1-s56.csv.bz2 /mnt/usb/combined-48s-r2-s60.csv.bz2 /mnt/usb/combined-48s-r3-output.csv.bz2 &
+nohup python3 main.py /mnt/usb/combined-48s-r1-s56.csv.bz2 /mnt/usb/combined-48s-r2-s60.csv.bz2 /mnt/usb/combined-48s-r3-output.csv.bz2 &
