4185425830f610fb869629862058fda1ebc70101
Jasmine Fan <lyspfan@gmail.com>
2026-02-21 18:09:32 -0500

    baby test file playground

--- test.py
+++ test.py
@@ -4,9 +4,8 @@ def main():
     ip_txt = "2607:5300::25b5"
     ip_obj = ipaddress.IPv6Address(ip_txt)
     print(f"{type(ip_obj)}")
-    raw = ip_obj.exploded.replace(":", "")
-    print(f"raw {raw}")
-
+    full = ip_obj.exploded.replace(":", "")
+    print(f"full form {full}")
 
 if __name__ == "__main__":
     main()
