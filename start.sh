da91bd29f9284f84a029763512f18c9cdaf00f69
Jasmine Fan <lyspfan@gmail.com>
2026-02-21 17:34:59 -0500

    fix bad file paths in bash script

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-python3 main.py /mnt/usb/ combined-48s-r1-s56.csv.bz2 combined-48s-r2-s60.csv.bz2 combined-48s-r3-output.csv.bz2 &
+python3 main.py /mnt/usb/combined-48s-r1-s56.csv.bz2 /mnt/usb/combined-48s-r2-s60.csv.bz2 /mnt/usb/combined-48s-r3-output.csv.bz2 &
