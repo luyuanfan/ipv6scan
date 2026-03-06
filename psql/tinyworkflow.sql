154950b5b060e65a1f61464b904c54dd093dc942
Jasmine Fan <lyspfan@gmail.com>
2026-03-06 16:54:50 -0500

    update workflow all the way up to getting entropy

--- psql/tinyworkflow.sql
+++ psql/tinyworkflow.sql
@@ -2,8 +2,6 @@ CREATE TABLE tinyRouterIPs (
     Protocol         boolean,    -- 1 for ICMP 0 for TCP
     TgtIP            text,       -- inet datatype ain't too versatile
     SrcIP            text,       -- inet datatype ain't too versatile
-    HostID           text,       -- host id of SrcIP
-    NetID            text,       -- network of SrcIP
     HopLim           smallint,   -- depends, prolly won't go over 32767
     ICMPv6Type       smallint,   -- 8 bits
     ICMPv6Code       smallint,   -- 8 bits
@@ -12,14 +10,25 @@ CREATE TABLE tinyRouterIPs (
     Entropy          float(24),  -- entropy score
     PfxLen           integer,    -- which subnet it's sitting on
     RouterType       boolean     -- 1 for yes 0 for no
-    -- might keep one file marking whether responding router uses v4 or v6
+    -- placeholder for HostID which will be inserted later
+    -- placeholder for NetID which will also be inserted later
 );
 
+-- add libraries
 CREATE EXTENSION plpython3u;
 
-INSERT INTO tinyRouterIPs VALUES (true, '2607:5380:1470:be58:8647:6e47:456e:bfc2', '2607:5380:8000:3e:1621:3ff:fedf:2ba8', NULL, NULL, 48, 1, 0, NULL, 125, NULL, 48, true);
-INSERT INTO tinyRouterIPs VALUES (true, '2607:fa49:1c42:3f18:8647:6e47:456e:bfc2', '2607:fa48:c:9500:64c2:882:e885:89bb', NULL, NULL, 46, 1, 0, NULL, 125, NULL, 48, true);
-INSERT INTO tinyRouterIPs VALUES (true, '2a01:cb08:9150:4a2b:8647:6e47:456e:bfc2', '2a01:cb08:9150:4a00:c2d7:aaff:febf:34f1', NULL, NULL, 239, 1, 0, NULL, 197, NULL, 48, true);
+-- define functions
+CREATE FUNCTION get_hid(addr text)
+  RETURNS text
+AS $$
+  return addr[64:]
+$$ LANGUAGE plpython3u;
+
+CREATE FUNCTION get_nid(addr text)
+  RETURNS text
+AS $$
+  return addr[:64]
+$$ LANGUAGE plpython3u;
 
 CREATE FUNCTION exploded(src_ip_str text)
   RETURNS text
@@ -29,9 +38,6 @@ AS $$
   full_addr = src_ip_obj.exploded.replace(":", "")
   return full_addr
 $$ LANGUAGE plpython3u;
-
-UPDATE tinyRouterIPs SET SrcIP = exploded(SrcIP);
-
 
 CREATE FUNCTION shannon_bin(hid text)
   RETURNS float
@@ -44,4 +50,21 @@ AS $$
   return score
 $$ LANGUAGE plpython3u;
 
-UPDATE tinyRouterIPs SET entropy = shannon_bin(SrcIP);
+-- insert records
+INSERT INTO tinyRouterIPs VALUES (true, '2607:5380:1470:be58:8647:6e47:456e:bfc2', '2607:5380:8000:3e:1621:3ff:fedf:2ba8', NULL, NULL, 48, 1, 0, NULL, 125, NULL, 48, true);
+INSERT INTO tinyRouterIPs VALUES (true, '2607:fa49:1c42:3f18:8647:6e47:456e:bfc2', '2607:fa48:c:9500:64c2:882:e885:89bb', NULL, NULL, 46, 1, 0, NULL, 125, NULL, 48, true);
+INSERT INTO tinyRouterIPs VALUES (true, '2a01:cb08:9150:4a2b:8647:6e47:456e:bfc2', '2a01:cb08:9150:4a00:c2d7:aaff:febf:34f1', NULL, NULL, 239, 1, 0, NULL, 197, NULL, 48, true);
+
+-- TODO: might wanna add a func that filter out v4 addresses
+--       but that can also be done through views i guess
+
+-- process ips into manageable state
+UPDATE tinyRouterIPs SET SrcIP = exploded(SrcIP);
+
+ALTER TABLE tinyRouterIPs ADD COLUMN HostID text;
+ALTER TABLE tinyRouterIPs ADD COLUMN NetID text;
+
+UPDATE tinyRouterIPs SET HostID = get_hid(SrcIP);
+UPDATE tinyRouterIPs SET NetID = get_nid(SrcIP);
+
+UPDATE tinyRouterIPs SET entropy = shannon_bin(HostID);
