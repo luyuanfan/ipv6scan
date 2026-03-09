8954b4b0fc0cbac5c3b17f5858ab4481151a5d8e
Jasmine Fan <lyspfan@gmail.com>
2026-03-08 20:07:45 -0400

    update import bash commands for small and tiny workflows

--- psql/bzimport.sh
+++ psql/bzimport.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-CSV="/mnt/usb/combined-48s-r1-s56.csv"
+BZ="/mnt/usb/combined-48s-r1-s56.csv.bz2"
+CSV="${BZ%.bz2}"
 
-# decompress file
-bzip2 -dk combined-48s-r1-s56.csv.bz2
+# decompress file (lbzip2 is the multithreaded version of bzip)
+lbzip2 -dk -n $(nproc) $BZ > /tmp/$CSV
 
 # truncate first four lines in file in-place (line numbering start at 1, which is weird): 
-sed -i -e 1,4d data/combined-48s-r1-s56.csv
+sed -i -e 1,4d /tmp/$CSV
 
 # load icmp rows (7 columns, with ICMPv6Type, ICMPv6Code, and NULL Flags col)
 psql -c "\COPY routerIPs (Protocol, TgtIP, SrcIP, HopLim, ICMPv6Type, ICMPv6Code, RTT) FROM STDIN WITH (FORMAT csv)"< <(grep '^icmp,' "$CSV")
--- psql/smallimport.sh
+++ psql/smallimport.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 CSV="data/small.csv"
 
-# need to decompress file
 # truncate first four lines in file in-place (line numbering start at 1, which is weird):
-sed -i -e 1,4d data/small.csv
+# sed -i -e 1,4d data/small.csv
 
 psql -h localhost -p 6789 -c "\COPY smallRouterIPs (Protocol, TgtIP, SrcIP, HopLim, ICMPv6Type, ICMPv6Code, RTT) FROM STDIN WITH (FORMAT csv)"< <(grep '^icmp,' "$CSV")
 
